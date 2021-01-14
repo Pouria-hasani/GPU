18e987d98c70823b8cb87e95f74219ce5facf4a9
Pouria-hasani <56039335+Pouria-hasani@users.noreply.github.com>
2021-01-15 01:11:43 +0300

    initial

---
+++ mnist.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as numpy
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    print("Name:", gpu.name, "  Type:", gpu.device_type)
+
+from tensorflow.python.client import device_lib
+
+device_lib.list_local_devices()
+
+tf.test.is_gpu_available()
+tf.debugging.set_log_device_placement(True)
+
+# Place tensors on the CPU
+with tf.device('/GPU:0'):
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+predictions = model(x_train[:1]).numpy()
+predictions
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test,  y_test, verbose=2)
+
